81461c9508230fc1dbad2e79b619fb6b56489850
Art Pelling <a.pelling@tu-berlin.de>
2025-09-12 13:47:00 +0200

    add plotting routine for directivities

---
+++ src/scripts/directivities.py
@@ -0,0 +1,22 @@
+import acoular as ac
+import numpy as np
+import spharpy as sph
+import matplotlib.pyplot as plt
+
+
+def plot_directivities() -> None:
+    sh_order = 5
+    coords = sph.samplings.equalarea(sh_order)
+    directivities = ac.directivity.Directivity.__subclasses__()
+    fig = plt.figure()
+    for directivity in directivities:
+        ax = fig.add_subplot(1, len(directivities), directivities.index(directivity) + 1, projection="3d", title=directivity.__name__)
+        coeffs = directivity(target_directions=coords.cartesian).coefficients
+        sph.plot.balloon_wireframe(coords, coeffs, ax=ax)
+
+    return fig
+
+
+if __name__ == "__main__":
+    fig = plot_directivities()
+    plt.show()
